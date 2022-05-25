c15a30b6a9ae08033fa64386cce2a67ed22044c6
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-25 17:06:35 -0300

    mini_project_9

---
+++ 9_contador_de_asteriscos/script.js
@@ -0,0 +1,9 @@
+contarAsterisco(10);
+
+function contarAsterisco(linhas){
+    let asterisco = '';
+    for(i = 0; i<linhas; i++){
+        asterisco += '*';
+        console.log(asterisco);
+    }
+}
