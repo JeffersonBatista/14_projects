0aee4e9f99654f701f2dbb763c11c83558e3f786
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-25 18:19:51 -0300

    mini_project_10

---
+++ 10_numeros_primos/script.js
@@ -0,0 +1,27 @@
+numerosPrimos(15);
+
+
+function numerosPrimos(limite){
+    for(let numero = 2; numero <= limite; numero++){
+        let ehPrimo = true;
+        
+        for(let divisor = 2; divisor < numero; divisor++){
+            if(numero % divisor === 0){
+                ehPrimo = false;
+                break;
+            }
+        }
+        if(ehPrimo) console.log(numero);
+    }
+}
+   
+/* 
+function numerosPrimos(limite){
+    for(let i=1; i<=limite; i++){
+        if(i%2 === 0){
+        }else{
+            console.log(i);
+        }
+    }
+}
+*/
