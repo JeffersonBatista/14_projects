231292336e03a5c432b2a39aa4dc9e7966cf1477
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-25 18:29:05 -0300

    otimização projeto 10

--- 10_numeros_primos/script.js
+++ 10_numeros_primos/script.js
@@ -1,5 +1,17 @@
 numerosPrimos(15);
 
+/* Primeira tentativa
+function numerosPrimos(limite){
+    for(let i=1; i<=limite; i++){
+        if(i%2 === 0){
+        }else{
+            console.log(i);
+        }
+    }
+}
+*/
+
+/* Segunda tentativa - Funcionou perfeitamente
 function numerosPrimos(limite){
     for(let numero = 2; numero <= limite; numero++){
         let ehPrimo = true;
@@ -13,14 +25,21 @@ function numerosPrimos(limite){
         if(ehPrimo) console.log(numero);
     }
 }
-   
-/* 
+ */  
+
+/* Otimização e reutilização do código */
 function numerosPrimos(limite){
-    for(let i=1; i<=limite; i++){
-        if(i%2 === 0){
-        }else{
-            console.log(i);
-        }
+    for(let numero = 2; numero <= limite; numero++){
+        if(ehPrimo(numero)) console.log(numero);
     }
 }
-*/
+
+function ehPrimo(numero){
+    for(let divisor = 2; divisor < numero; divisor++){
+            if(numero % divisor === 0){
+                return false;
+            }
+        }
+        return true;
+}
+//Cada função fica responsável por resolver um problema
