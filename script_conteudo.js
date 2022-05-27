aa7c3fef57912ded204e20631cdb83df2e86a9da
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-27 17:40:51 -0300

    manipulando_strings

--- script_conteudo.js
+++ script_conteudo.js
@@ -263,3 +263,9 @@ const celular3 = Object.assign({
     tipoFone : 'bluetoth'
 },celular);
 console.log(celular3);//Vai herdar todas as propriedades do objeto principal + a que foi criada no clone
+//Funções matemáticas
+console.log(Math.random());//Gera um número aleatório de 0 á 1
+console.log(Math.max(3,6,9,12,15,18,21));//Mostra o maior número dentro dos números de parâmetro
+console.log(Math.min(3,6,9,12,15,18,21));//Mostra o menor número dentro dos números de parâmetro
+console.log(Math.pow(3,2))//Retorna o primeiro elevado a quantidade de vezes do segundo
+console.log(Math.toSource(32));//Retorna uma string do valor
--- script_test.js
+++ script_test.js
@@ -1,6 +1,21 @@
-//Funções matemáticas
-console.log(Math.random());//Gera um número aleatório de 0 á 1
-console.log(Math.max(3,6,9,12,15,18,21));//Mostra o maior número dentro dos números de parâmetro
-console.log(Math.min(3,6,9,12,15,18,21));//Mostra o menor número dentro dos números de parâmetro
-console.log(Math.pow(3,2))//Retorna o primeiro elevado a quantidade de vezes do segundo
-console.log(Math.toSource(32));//Retorna uma string do valor
+//Strings
+//Tipo Primitivo
+const mensagem = ' Olá eu me chamo Jefferson';
+//Tipo objeto
+const mensagem2 = new String('Nasci em 1990');
+console.log(mensagem);
+console.log(typeof mensagem);//Tipo
+console.log(mensagem2);
+console.log(typeof mensagem2);
+console.log(mensagem.length);//Tamanho da String
+console.log(mensagem2.length);
+console.log(mensagem[1]);//O que está na posição do index
+console.log(mensagem2[0]);
+console.log(mensagem.includes('Olá'));//Se a string contém
+console.log(mensagem.includes('Nascido'));
+console.log(mensagem.startsWith('Olá'));//Se a string inicia com ''
+console.log(mensagem.endsWith('nome'));//Se a string finaliza com ''
+console.log(mensagem.indexOf('J'));//Informa o index de onde se encontra a letra
+console.log(mensagem.trim());//Remove os espaços excedentes no início ou fim da String
+console.log(mensagem.replace('Jefferson', 'Priscila'));//Troca uma palavra pela outra
+console.log(mensagem.split(' '));//Separa a string pelo valor informado, no caso espaços
